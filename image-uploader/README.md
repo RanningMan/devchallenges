ccd801c40aa9ddf4d6e5c8ac1746ce4859029363
RanningMan <xiaranforusa@gmail.com>
2022-11-03 23:46:53 -0700

    client work done

--- image-uploader/README.md
+++ image-uploader/README.md
@@ -8,11 +8,11 @@
 
 <div align="center">
   <h3>
-    <a href="https://{your-demo-link.your-domain}">
+    <a href="https://devchallenge-image-uploader.netlify.app/">
       Demo
     </a>
     <span> | </span>
-    <a href="https://{your-url-to-the-solution}">
+    <a href="https://github.com/RanningMan/devchallenges/tree/main/image-uploader">
       Solution
     </a>
     <span> | </span>
@@ -37,12 +37,57 @@
 ## Overview
 
 [architecture](./architecture.svg)
+<img src='./architecture.svg' alt='architecture' />
 
 Learnings:
 
 1. how to hide file upload input
+```
+  input {
+    z-index: -1;
+    width: 0;
+    height: 0;
+    position: relative;
+    overflow: hidden;
+    opacity: 0;
+  }
+```
 
 2. how to use file upload input
+```
+<input type="file" onChange={onChange} />
+
+<script>
+  onChange(e) {
+    var files = e.target.files;
+    // do stuff
+  }
+</script>
+```
+
+3. how to drag and drop file to upload  
+https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop 
+
+4. Clipboard API
+There are three primary browser APIs for copying to the clipboard:
+
+    **4.1 [Async Clipboard API](https://www.w3.org/TR/clipboard-apis/#async-clipboard-api) [navigator.clipboard.writeText]**
+  - Text-focused portion available in Chrome 66 (March 2018)
+  - Access is asynchronous and uses JavaScript Promises, can be written so security user prompts (if displayed) don't interrupt the JavaScript in the page.
+  - Text can be copied to the clipboard directly from a variable.
+  - Only supported on pages served over HTTPS.
+  - In Chrome 66 pages inactive tabs can write to the clipboard without a permissions prompt.
+
+    **4.2 document.execCommand('copy') ([deprecated](https://developer.mozilla.org/docs/Web/API/Document/execCommand#browser_compatibility)) 👎**
+  - Most browsers support this as of ~April 2015 (see Browser Support below).
+  - Access is synchronous, i.e. stops JavaScript in the page until complete including displaying and user interacting with any security prompts.
+  - Text is read from the DOM and placed on the clipboard.
+  - During testing ~April 2015 only Internet Explorer was noted as displaying permissions prompts whilst writing to the clipboard.
+
+    **4.3 Overriding the copy event**
+  - See Clipboard API documentation on [Overriding the copy event](https://w3c.github.io/clipboard-apis/#override-copy).
+  - Allows you to modify what appears on the clipboard from any copy event, can include other formats of data other than plain text.
+  - Not covered here as it doesn't directly answer the question.
 
 ### Built With
 
--- image-uploader/src/App.js
+++ image-uploader/src/App.js
@@ -1,25 +1,176 @@
+import { useState } from 'react';
 import './App.css';
 import DnD from './image.svg';
 
-function App() {
+const AppState = {
+	UPLOAD: 'upload',
+	UPLOADING: 'uploading',
+	UPLOADED: 'uploaded',
+};
+
+function UploadContent({ onFileDrop, onFileDragOver, onFileSelected }) {
 	return (
-		<div className='app'>
+		<>
 			<header className='header'>Upload your image</header>
 			<div className='subtitle'>File should be Jpeg, png, ...</div>
-			<div className='dragAndDropBox'>
-				<input type='file' id='dragAndDrop' />
+			<div
+				className='dragAndDropBox'
+				onDrop={onFileDrop}
+				onDragOver={onFileDragOver}
+			>
+				<input
+					type='file'
+					id='dragAndDrop'
+					accept='image/png, image/jpeg'
+					onChange={onFileSelected}
+					className='hidden'
+				/>
 				<label htmlFor='dragAndDrop' className='dragAndDropBackground'>
 					<img src={DnD} alt='drag and drop' />
 					<p>Drag & Drop your image here</p>
 				</label>
 			</div>
 			<div className='linebreaker'>Or</div>
-			<div className='uploadButton'>
-				<input type='file' id='file' />
-				<label for='file'>Choose a file</label>
+			<div className='button'>
+				<input
+					type='file'
+					id='file'
+					accept='image/png, image/jpeg'
+					onChange={onFileSelected}
+					className='hidden'
+				/>
+				<label htmlFor='file'>Choose a file</label>
+			</div>
+		</>
+	);
+}
+
+function Uploading() {
+	return (
+		<div>
+			<h2 className='header'>Uploading...</h2>
+			<div className='emptyBar'>
+				<hr className='blueBar' />
 			</div>
 		</div>
 	);
+}
+
+function Uploaded({ imageToUpload, uploadedUrl }) {
+	const onCopy = () => {
+		navigator.clipboard.writeText(uploadedUrl).then(() => {
+			window.confirm('Copied!');
+		});
+	};
+	return (
+		<>
+			<div className='checkmark'>
+				<span className='material-symbols-outlined check'>done</span>
+			</div>
+			<header className='header'>Uploaded Successfully!</header>
+			<div className='dragAndDropBox'>
+				<div className='dragAndDropBackground'>
+					<img src={imageToUpload} alt='uploaded successfully' />
+				</div>
+			</div>
+			<div className='imageUrl'>
+				<input type='text' readOnly value={uploadedUrl} />
+				<button className='button copyButton' onClick={onCopy}>
+					CopyLink
+				</button>
+			</div>
+		</>
+	);
+}
+
+const getFileToUploadOnDrop = (dropEvent) => {
+	if (dropEvent.dataTransfer.items) {
+		if (dropEvent.dataTransfer.items.length > 1) {
+			throw Error('Does not support uploading multiple images');
+		}
+		const item = dropEvent.dataTransfer.items[0];
+		if (item.kind === 'file') {
+			return item.getAsFile();
+		}
+	} else {
+		if (dropEvent.dataTransfer.files.length > 1) {
+			throw Error('Does not support uploading multiple images');
+		}
+		return dropEvent.dataTransfer.files[0];
+	}
+	return null;
+};
+
+const wait = (seconds = 3000) => {
+	return new Promise((resolve) =>
+		setTimeout(() => {
+			resolve();
+		}, seconds)
+	);
+};
+
+const uploadFile = async (file) => {
+	alert(file.name);
+	await wait();
+	return Promise.resolve({ url: 'https://image.com', image: file });
+};
+
+function App() {
+	const [appState, setAppState] = useState(AppState.UPLOAD);
+	const [imageToUpload, setImageToUpload] = useState('');
+	const [uploadedUrl, setUploadedUrl] = useState('');
+
+	const onFileDragOver = (e) => {
+		e.preventDefault();
+	};
+
+	const uploadImage = async (fileToUpload) => {
+		if (fileToUpload) {
+			setAppState(AppState.UPLOADING);
+			const { url, image } = await uploadFile(fileToUpload);
+			setUploadedUrl(url);
+			setImageToUpload(image);
+			setAppState(AppState.UPLOADED);
+		}
+	};
+
+	const onImageDrop = async (e) => {
+		e.preventDefault();
+		try {
+			const fileToUpload = getFileToUploadOnDrop(e);
+			await uploadImage(fileToUpload);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	const onImageSelected = async (e) => {
+		const files = e.target.files;
+		if (files.length > 1) {
+			console.log('Does not support uploading multiple images');
+		} else {
+			const fileToUpload = files[0];
+			await uploadImage(fileToUpload);
+		}
+	};
+
+	let content = null;
+	if (appState === AppState.UPLOAD) {
+		content = (
+			<UploadContent
+				onFileDragOver={onFileDragOver}
+				onFileDrop={onImageDrop}
+				onFileSelected={onImageSelected}
+			/>
+		);
+	} else if (appState === AppState.UPLOADING) {
+		content = <Uploading />;
+	} else if (appState === AppState.UPLOADED) {
+		content = (
+			<Uploaded imageToUpload={imageToUpload} uploadedUrl={uploadedUrl} />
+		);
+	}
+	return <div className='app'>{content}</div>;
 }
 
 export default App;
